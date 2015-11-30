69127ccee44f40b5496b37524473fed49c408cc8
Adam Melkus <adam@melkus.info>
2015-12-01 00:23:25 +0100

    fixed production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,15 @@ Rails.application.configure do
                                               exception_recipients: %w{adam@melkus.info}
                                           }
 
+  config.action_mailer.default_url_options = { :host => 'warm-wave-1682.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+      :address        => "smtp.sendgrid.net",
+      :port           => "25",
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => ENV['SENDGRID_DOMAIN']
+  }
+
 
 end
