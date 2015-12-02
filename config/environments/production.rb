adb25adc14a89a35f26ee69c4154c9d62015587a
Adam Melkus <adam@melkus.info>
2015-12-02 14:04:25 +0100

    paperclip aws integration fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,23 +85,20 @@ Rails.application.configure do
                                               exception_recipients: %w{adam@melkus.info}
                                           }
 
-  config.action_mailer.default_url_options = { :host => ENV['EMAIL_HOST'] }
+  config.action_mailer.default_url_options = {:host => ENV['EMAIL_HOST']}
   ActionMailer::Base.smtp_settings = {
-      :address        => "smtp.sendgrid.net",
-      :port           => "25",
+      :address => "smtp.sendgrid.net",
+      :port => "25",
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => ENV['SENDGRID_DOMAIN']
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => ENV['SENDGRID_DOMAIN']
   }
 
   config.paperclip_defaults = {
       :storage => :s3,
-      :s3_credentials => {
-          :bucket => ENV['S3_BUCKET_NAME'],
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :s3_host_name => 's3.eu-central-1.amazonaws.com'
   }
 
 
